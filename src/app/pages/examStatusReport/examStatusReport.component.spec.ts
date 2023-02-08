02c2181ec301afd0f3c2ac3554d7b309d190972f
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2023-02-08 15:48:34 +0530

    added exam status component

---
+++ src/app/pages/examStatusReport/examStatusReport.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ExamStatusReportComponent } from './examStatusReport.component';
+
+describe('ExamStatusReportComponent', () => {
+  let component: ExamStatusReportComponent;
+  let fixture: ComponentFixture<ExamStatusReportComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExamStatusReportComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExamStatusReportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/examStatusReport/examStatusReport.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examStatusReport',
+  templateUrl: './examStatusReport.component.html',
+  styleUrls: ['./examStatusReport.component.css']
+})
+export class ExamStatusReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
