f8cc3e76f208c3928c6cc03004f01214f8c08187
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2023-02-13 18:09:57 +0530

    hud added

---
+++ src/app/pages/dashboardHUD/dashboardHUD.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardHUD',
+  templateUrl: './dashboardHUD.component.html',
+  styleUrls: ['./dashboardHUD.component.css']
+})
+export class DashboardHUDComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/dashboardHUD/dashboardHUD.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardHUDComponent } from './dashboardHUD.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DashboardHUDComponent]
+})
+export class DashboardHUDModule { }
