51bfe44e9188bd752c76343ef256a936845916c0
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2023-07-27 15:14:29 +0530

    added tabs

---
+++ src/app/pages/commons/tabHead/tab-head.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tab-head',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tab-head.component.html',
+  styleUrls: ['./tab-head.component.scss'],
+})
+export class TabHeadComponent {}
