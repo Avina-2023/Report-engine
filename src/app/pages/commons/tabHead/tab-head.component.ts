65f5b98c4ed0e1201636e59f900dd42c4c1f3a92
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2023-08-02 18:03:38 +0530

    header tabs

--- src/app/pages/commons/tabHead/tab-head.component.ts
+++ src/app/pages/commons/tabHead/tab-head.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tab-head',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './tab-head.component.html',
   styleUrls: ['./tab-head.component.scss'],
 })
