1736db1d04df17a4a3dd1736695821fdd27a2fbd
Rooshitha P <rooshitha.p@lntecc.com>
2023-09-16 09:06:32 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://reportedge.lntedutech.com',
+  API_BASE_URL: 'https://reportedgedr.lntedutech.com',
   ELASTIC_BASE_URL: 'https:///reportelastic.lntedutech.com',
   local:'http://localhost:3006',
   kibana_url:'https://kibanaprod.lntedutech.com/',
